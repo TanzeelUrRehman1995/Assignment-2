e6306302c9089f7796de32aff608fe91db1a6ab2
TanzeelUrRehman1995 <>
2023-08-12 20:41:18 +0500

    Question no 1

---
+++ app.js
@@ -0,0 +1,8 @@
+var num = 10;
+
+document.write(`Result: <br> The value of a is: ${num} <br> ====================== <br> <br>`);
+
+document.write(`The value of ++num is: ${++num} <br> Now the value of num is: ${num} <br> <br>`);
+document.write(`The value of ++num is: ${num++} <br> Now the value of num is: ${num} <br> <br>`);
+document.write(`The value of ++num is: ${--num} <br> Now the value of num is: ${num} <br> <br>`);
+document.write(`The value of ++num is: ${num--} <br> Now the value of num is: ${num} <br> <br>`);
